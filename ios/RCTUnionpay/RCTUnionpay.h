b013597676a97dcfa93a60d4e40795bae0d3af4b
liop <tmdqq01@gmail.com>
2018-07-16 14:11:33 +0800

    适配react-native 0.56.0

--- ios/RCTUnionpay/RCTUnionpay.h
+++ ios/RCTUnionpay/RCTUnionpay.h
@@ -8,10 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTEventEmitter.h>
+#else
 #import "RCTBridgeModule.h"
 #import "RCTLog.h"
 #import "RCTEventDispatcher.h"
 #import "RCTEventEmitter.h"
+#endif
+
 #import "UPPaymentControl.h"
 #define URL_SCHEMES_NOT_DEFINED (@"URL scheme is not defined!")
 #define START_PAY_FAILED (@"start pay failed")
